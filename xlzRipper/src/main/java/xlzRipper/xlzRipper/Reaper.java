680dbae26933e39e8d3b15a08ab7e12ce7dddfbc
Rafal.Krakiewicz <Rafal.Krakiewicz@WAW-JT9NV02.corpnet.liox.org>
2019-02-18 13:09:40 +0100

    dsa

--- xlzRipper/src/main/java/xlzRipper/xlzRipper/Reaper.java
+++ xlzRipper/src/main/java/xlzRipper/xlzRipper/Reaper.java
@@ -42,6 +42,8 @@ public class Reaper extends JFrame {
 	 * Create the frame.
 	 */
 	public Reaper() {
+		setTitle("Reaper");
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
